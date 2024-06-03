c66563df7f59acf0b92a4c9e69327888a71ff3d9
TIMMY5484 <130188416+TIMMY5484@users.noreply.github.com>
2024-06-02 21:03:13 -0400

    back and forth

--- script.lua
+++ script.lua
@@ -1,6 +1,14 @@
 local funky = get("text-div")
 local text = get("text")
+local i = 0
 
 funky.on_click(function()
-    text.set_opacity(0.5)
+    if i = 0 then
+        text.set_opacity(0.5)
+        i = 1
+    end
+    if i = 1 then
+        text.set_opacity(1)
+        i = i
+    end
 end)
