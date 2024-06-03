2d2bb1e1bb789fc5ceabc4145a8552426772c644
TIMMY5484 <130188416+TIMMY5484@users.noreply.github.com>
2024-06-02 21:29:30 -0400

    hover thing

--- script.lua
+++ script.lua
@@ -2,12 +2,6 @@ local funky = get("text-div")
 local text = get("text")
 local i = 0
 
-funky.on_click(function()
-    if i=0 then
-        text.set_opacity(0.5)
-        i = 1
-    elseif i=1 then
-        text.set_opacity(1)
-        i = i
-    end
+funky.on_over(function()
+    text.set_opacity(0.5)
 end)
